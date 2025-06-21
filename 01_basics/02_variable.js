758c0c21972bbd9a9d8dee6bb36b03d1b0acd424
Arpan-q <arpanghosh8691@gmail.com>
2025-06-22 00:36:32 +0530

    variable in JS

---
+++ 01_basics/02_variable.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) //we are using nodejs,not browser
+
+console.log(3
+    +3)     //code readable should be high
+    
+
+    let name ="Arpan"
+    let age = 19
+    let isLogginIn = false
+
+
+    //number => 2 to power 53
+    // bright
+    // string=>""
+    // boolean =>true/false
+    // null =>standalone data
+    // undefind
+    // symbol =>unique
+
+
+    // object
+
+    console.log(typeof 56)
