9f3357e844613ffb4cb563d553c8832faff98cc4
Arpan-q <arpanghosh8691@gmail.com>
2025-06-30 20:27:28 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,10 +2,10 @@ let score =' 33'
 
 // const {score} = req.body
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
+//console.log(typeof valueInNumber)
 
 
 
@@ -17,12 +17,20 @@ console.log(typeof valueInNumber)
 let isLoggedIn = "Arpan"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn )
+//console.log(booleanIsLoggedIn )
 
 //1=> true; 0 => false
 // "" =>false; "arpan"=>true
 
 let someNumber =33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+// ************************* Operations *****************************
+
+let value = 3
+let negValue = - value
+console.log(negValue)
